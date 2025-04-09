1763b5921e2933ff5d56a7cdf354add1e22b9eed
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-04-09 10:07:36 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -7,7 +7,7 @@ eval(fs.readFileSync('code.js')+'');
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
 
-
+//Property test comparing both implementations
 const test =
     jsc.forall("nat", function(n) {
         return dynamicEuler(n) === e(n);
